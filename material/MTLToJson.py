e50ee4e7fc7c99cdfa3446b0411e83006f2aa019
Bertrand Rix <bertrand.rix@gmail.com>
2019-10-01 12:08:41 +0200

    Add MTLToJson processing python script : Read a .mtl and output .json file handled by MeshLib.

---
+++ material/MTLToJson.py
@@ -0,0 +1,137 @@
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("inputMtl")
+parser.add_argument("outputJson")
+args = parser.parse_args()
+
+filepath = args.inputMtl
+outputFilePath = args.outputJson
+
+class Material:
+  def __init__(self, name, attributeUniformMap):
+    self.name = name
+    self.floatAttributes = dict()
+    self.vec3Attributes = dict()
+    self.textureAttributes = dict()
+    self.attributeUniformMap = attributeUniformMap;
+
+  def __repr__(self):
+    return str(self)
+  def __str__(self):
+    stringRep = self.name
+    stringRep += '\n'
+    stringRep += str(self.floatAttributes)
+    stringRep += '\n'
+    stringRep += str(self.vec3Attributes)
+    stringRep += '\n'
+    stringRep += str(self.textureAttributes)
+    return stringRep
+
+  def ToDict(self):
+
+    materialObject = dict()
+    materialObject["Shader"] = dict()
+
+    if "map_Kd" in self.textureAttributes :
+      if "map_bump" in self.textureAttributes :
+        materialObject["Shader"]["name"] = "NormalMapped"
+      else:
+        materialObject["Shader"]["name"] = "Textured"
+    else:
+      materialObject["Shader"]["name"] = "Colored"
+
+    materialObject["Attributes"] = list()
+
+    for att in self.floatAttributes:
+      if att in self.attributeUniformMap:
+        cAttribute = dict()
+        cAttribute["name"] = att
+        cAttribute["type"] = "float"
+        cAttribute["value"] = self.floatAttributes[att]
+        cAttribute["uniform"] = self.attributeUniformMap[att]
+        materialObject["Attributes"].append(cAttribute)
+
+    for att in self.vec3Attributes:
+      if att in self.attributeUniformMap:
+        cAttribute = dict()
+        cAttribute["name"] = att
+        cAttribute["type"] = "vec3"
+        cAttribute["value"] = self.vec3Attributes[att]
+        cAttribute["uniform"] = self.attributeUniformMap[att]
+        materialObject["Attributes"].append(cAttribute)
+
+    for att in self.textureAttributes:
+      if att in self.attributeUniformMap:
+        cAttribute = dict()
+        cAttribute["name"] = att
+        cAttribute["type"] = "UCharTex"
+        cAttribute["value"] = self.textureAttributes[att]
+        cAttribute["uniform"] = self.attributeUniformMap[att]
+        materialObject["Attributes"].append(cAttribute)
+
+    return materialObject
+def ParseFloat(p_floatStr):
+  return float(p_floatStr)
+
+def ParserVec3(p_floatStr):
+  valueTable = p_floatStr.split(' ')
+  return [float(valueTable[0]), float(valueTable[1]), float(valueTable[2])]
+
+currentMaterial = None
+materialList = []
+
+UniformMap = dict()
+UniformMap["Ka"] = "ambient_color"
+UniformMap["Kd"] = "diffuse_color"
+UniformMap["Ks"] = "specular_color"
+UniformMap["map_Kd"] = "texture1"
+UniformMap["map_Ka"] = "texture2"
+UniformMap["map_Ks"] = "texture3"
+UniformMap["map_bump"] = "texture4"
+UniformMap["map_d"] = "texture5"
+
+FloatToParse = ["Ns", "Ni", "d", "Tr", "illum"]
+Vec3ToParse = ["Tf", "Ka", "Kd", "Ks", "Ke"]
+TextureToPars = ["map_Ka", "map_Kd", "map_d", "map_bump"]
+
+
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+     #Remove last char of line
+     line = line[:-1]
+     if line.startswith("newmtl"):
+       currentMtlName = line[7:]
+       print currentMtlName
+       newMTL = Material(currentMtlName, UniformMap)
+       materialList.append(newMTL)
+       currentMaterial = newMTL
+
+     elif currentMaterial != None and len(line) > 0 :
+       #First char is a tab
+       line = line[1:]
+       #print line.split(' ', 1)
+       attributeName = line.split(' ', 1)[0]
+       #print attributeName
+       attributeValue = line.split(' ', 1)[1]
+       #print attributeValue
+       if attributeName in FloatToParse:
+         currentMaterial.floatAttributes[attributeName] = ParseFloat(attributeValue)
+       if attributeName in Vec3ToParse:
+         currentMaterial.vec3Attributes[attributeName] = ParserVec3(attributeValue)
+       if attributeName in TextureToPars:
+         currentMaterial.textureAttributes[attributeName] = attributeValue
+
+     line = fp.readline()
+
+
+#Serialize all material
+jsonRep = dict()
+for mat in materialList:
+  jsonRep[mat.name] = mat.ToDict()
+
+with open(outputFilePath, "w") as write_file:
+    json.dump(jsonRep, write_file, indent=4)
